fff61b5bd1fcd348f32c147a4b385564af807370
andrewJamesBaker <61355438+andrewJamesBaker@users.noreply.github.com>
2021-02-02 12:19:18 -0500

    Fiddled around with sensor data, trying to replicate the tutorial I started, but ended up doing my own thing. Started work using expo's sensor api, which seems like it may work a little easier. =

--- App.js
+++ App.js
@@ -2,12 +2,36 @@ import React, { useState, Component } from 'react';
 import { StatusBar, StyleSheet, View, FlatList, Alert, TouchableWithoutFeedback, Keyboard, Text } from 'react-native';
 import Header from './components/header';
 import Card from './components/card';
-import { accelerometer, gyroscope} from "react-native-sensors";
+// import { accelerometer, gyroscope, setUpdateIntervalForType, SensorTypes} from "react-native-sensors";   THIS SHOULD SOON BE UNNECESSARY. EXPO SENSORS.
+import { GameEngine } from "react-native-game-engine";
+import * as Sensors from 'expo-sensor';
+// import Matter from "matter.js";      //From the tutorial that I was using. Might be useful during game dev.
+// import randomInt from "random-int";
+// import randomColor from "randomcolor";
+
+
 export default function App() {
 
   const [todos, setTodos] = useState([
     { text: 'Go to team meeting', key: '1' },
   ]);
+
+  const [data, setData] = useState({
+    x: 0,
+    y: 0,
+    z: 0,
+  });
+  const [subscription, setSubscription] = useState(null);
+
+  const _subscribe = () => {
+    setSubscription(
+      Accelerometer.addListener(accelerometerData => {
+        setData(accelerometerData);
+      })
+    );
+  };
+
+  const { x, y, z } = data;
 
   return (
 
