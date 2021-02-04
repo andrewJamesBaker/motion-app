a4594926d7580d9ab657c85a8b4cd73a7d10fd23
andrewJamesBaker <61355438+andrewJamesBaker@users.noreply.github.com>
2021-02-04 12:23:04 -0500

    Sensor data now works. Working on accessing accelerometer and gyroscope data, not going super. Currently non functional

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
-import React, { useState, Component } from 'react';
+import React, { useState, Component, useEffect } from 'react';
 import { StatusBar, StyleSheet, View, FlatList, Alert, TouchableWithoutFeedback, Keyboard, Text } from 'react-native';
 import Header from './components/header';
 import Card from './components/card';
 // import { accelerometer, gyroscope, setUpdateIntervalForType, SensorTypes} from "react-native-sensors";   THIS SHOULD SOON BE UNNECESSARY. EXPO SENSORS.
 import { GameEngine } from "react-native-game-engine";
-import * as Sensors from 'expo-sensor';
+import { Accelerometer, Gyroscope } from 'expo-sensors';
 // import Matter from "matter.js";      //From the tutorial that I was using. Might be useful during game dev.
 // import randomInt from "random-int";
 // import randomColor from "randomcolor";
@@ -16,34 +16,89 @@ export default function App() {
     { text: 'Go to team meeting', key: '1' },
   ]);
 
-  const [data, setData] = useState({
-    x: 0,
-    y: 0,
-    z: 0,
-  });
-  const [subscription, setSubscription] = useState(null);
 
-  const _subscribe = () => {
-    setSubscription(
+  // Bellow taken from https://docs.expo.io/versions/latest/sdk/accelerometer/#api
+  const [dataAcc, setDataAcc] = useState({
+    xa: 0,
+    ya: 0,
+    za: 0,
+  });
+
+  const [dataGyro, setDataGyro] = useState({
+    xg: 0,
+    yg: 0,
+    zg: 0,
+  });
+
+  const [subscriptionAcc, setSubscriptionAcc] = useState(null);
+  const [subscriptionGyro, setSubscriptionGyro] = useState(null);
+
+  Accelerometer.setUpdateInterval(500);
+
+  const _subscribeAccelerometer = () => {
+    setSubscriptionAcc(
       Accelerometer.addListener(accelerometerData => {
-        setData(accelerometerData);
+        setDataAcc(accelerometerData);
       })
     );
   };
 
-  const { x, y, z } = data;
+  const _subscribeGyro = () => {
+    setSubscriptionGyro(
+      Gyroscope.addListener(gyroscopeData => {
+        setDataGyro(gyroscopeData);
+      })
+    );
+  };
+
+  const _unsubscribe = () => {
+    subscriptionAcc && subscriptionAcc.remove();
+    setSubscriptionAcc(null);
+    subscriptionGyro && subscriptionGyro.remove();
+    setSubscriptionGyro(null);
+  };
+
+  useEffect(() => {
+    _subscribeAccelerometer();
+    _subscribeGyro();
+    return () => _unsubscribe();
+  }, []);
+
+  const { xa, ya, za } = dataAcc;
+  const { xg, yg, zg } = dataGyro;
 
   return (
 
-    
+
     <TouchableWithoutFeedback>
       <View style={styles.container}>
-        <Card><Text>Hello</Text></Card>
-        <StatusBar hidden = {true}/>
+        <Card>
+          <Text>Hello</Text>
+        </Card>
+        <Card>
+          <Text style={styles.text}>Accelerometer: (in Gs where 1 G = 9.81 m s^-2)</Text>
+          <Text style={styles.text}>
+            x: {round(xa)} y: {round(ya)} z: {round(za)}
+          </Text>
+        </Card>
+        <Card>
+          <Text style={styles.text}>Gyroscope:</Text>
+          <Text style={styles.text}>
+            x: {round(xg)} y: {round(yg)} z: {round(zg)}
+          </Text>
+        </Card>
+        <StatusBar hidden={true} />
       </View>
     </TouchableWithoutFeedback>
-    
+
   );
+}
+
+function round(n) {
+  if (!n) {
+    return 0;
+  }
+  return Math.floor(n * 100) / 100;
 }
 
 const styles = StyleSheet.create({
@@ -56,5 +111,8 @@ const styles = StyleSheet.create({
   },
   list: {
     marginTop: 20,
+  },
+  text: {
+    textAlign: 'center',
   }
 })
--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # motion-app
+## Written by Andrew Baker
+
+Written in React Native, using Expo.
+
+Combination app mixing elements from the Nintendo Wii or Xbox Kinect, basic mobile games, and screen sharing software. 
+
+Current state: Hardware mode
+- No graphics: Currently there are no end user graphics. 
+- Few apis: Extra elements of the app are being added slowly. Sensor data is starting to work, but the game engine and matter module are not implemented. 
+- Algorithms to find user position and general controls
+
+Completed:
+- Landscape orientation
+- Android navigation bar removed
+- Notification bar removed
